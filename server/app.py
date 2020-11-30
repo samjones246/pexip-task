1600238c37b9048514b0bfab97fafef7ce7a2488
Sam Jones <sam.jones240699@gmail.com>
2020-11-30 23:57:25 +0000

    initialised server as flask app

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    data = request.get_json()
+    print("received data")
+    return "\n".join(data["added"], data["removed"], data["changed"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
