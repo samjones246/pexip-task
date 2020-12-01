452dc722c0339b0de8854a54cf5fd6319850a4c2
Sam Jones <sam.jones240699@gmail.com>
2020-12-01 00:50:34 +0000

    restructured server to perform different actions based on http method used

--- server/app.py
+++ server/app.py
@@ -2,10 +2,18 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
-def main():
+def add_files():
     data = request.get_json()
     print(data)
     return "received"
+
+@app.route("/", methods=["DELETE"])
+def remove_files():
+    pass
+
+@app.route("/", methods=["PUT"])
+def update_files():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
