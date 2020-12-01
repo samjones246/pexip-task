1107b84046c5c324e4f5734052f0e8cbbd99fa63
Sam Jones <sam.jones240699@gmail.com>
2020-12-01 02:13:21 +0000

    removed flask elements from server, ready to switch to sockets

--- server/app.py
+++ server/app.py
@@ -1,19 +1,24 @@
-from flask import Flask, request
-app = Flask(__name__)
+import argparse
+import socketserver
 
-@app.route("/", methods=["POST"])
-def add_files():
-    data = request.get_json()
-    print(data)
-    return "received"
+def main():
+    parser = argparse.ArgumentParser(description="Update a folder with changes received from a client application")
+    parser.add_argument("path", type=str, help="Path to the folder which should be synchronised.\
+                                                Can be relative to your current working directory or absolute.")
+    parser.add_argument("--host", type=str, help="Address to bind the server to (default: 0.0.0.0)",
+                        default="0.0.0.0")
+    parser.add_argument("--port", type=int, help="Port to open the server on (default: 5000)",
+                        default=5000)
+    args = parser.parse_args()
 
-@app.route("/", methods=["DELETE"])
-def remove_files():
+def add_files(files):
     pass
 
-@app.route("/", methods=["PUT"])
-def update_files():
+def remove_files(filenames):
+    pass
+
+def update_files(files):
     pass
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
